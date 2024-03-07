4cd65ca90ae8cfe9829f7c18958a7d87247ce35c
MindaugasK <mindaugas.krup@gmail.com>
2024-03-07 21:23:08 +0200

    update import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApiKeysModule } from './api-key/api-key.module';
 
 @Module({
-  imports: [],
+  imports: [ApiKeysModule],
   controllers: [AppController],
   providers: [AppService],
 })
